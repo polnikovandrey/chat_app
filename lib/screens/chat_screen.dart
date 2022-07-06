51998feb43e3287579bab8bd108230d108eab23e
Andrey Polnikov <polnikov.andrey@gmail.com>
2022-07-06 22:34:48 +0300

    + firebase sdk basic usage

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -15,6 +17,16 @@ class ChatScreen extends StatelessWidget {
           padding: const EdgeInsets.all(8),
           child: const Text('This works'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final firebaseApp = await Firebase.initializeApp();
+          final firestore = FirebaseFirestore.instanceFor(app: firebaseApp);
+          final chatsCollectionSnapshot = await firestore.collection('chats').get();
+          final messagesCollectionSnapshot = await chatsCollectionSnapshot.docs.first.reference.collection('messages').get();
+          messagesCollectionSnapshot.docs.forEach((document) => print(document.data()['text']));
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
