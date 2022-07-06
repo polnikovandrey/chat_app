4d6276d9b4abbff8588c440f98409536f9adad85
Andrey Polnikov <polnikov.andrey@gmail.com>
2022-07-06 20:29:50 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const ChatApp());
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
