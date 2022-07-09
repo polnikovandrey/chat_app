7d2b411919d7e9d7c5b6f67d4908ab10643b5b8f
Andrey Polnikov <polnikov.andrey@gmail.com>
2022-07-09 20:21:42 +0300

    + PushNotifications basics

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,32 @@ import 'package:chat_app/screens/auth_screen.dart';
 import 'package:chat_app/screens/chat_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  FirebaseMessaging.onMessage.listen((message) {
+    print(message.toMap());
+  });
+  FirebaseMessaging.onMessageOpenedApp.listen((message) {
+    print(message.toMap());
+  });
   runApp(const ChatApp());
 }
 
