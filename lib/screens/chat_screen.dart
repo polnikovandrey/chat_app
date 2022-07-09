f41a1ca7a98771d3faa56b9cdb2963ef2a366bc5
Andrey Polnikov <polnikov.andrey@gmail.com>
2022-07-09 13:10:30 +0300

    + authentication & logout logic

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -9,6 +10,33 @@ class ChatScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chat'),
+        actions: [
+          DropdownButton(
+            icon: const Icon(Icons.more_vert),
+            items: [
+              DropdownMenuItem(
+                value: 'logout',
+                child: Container(
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.exit_to_app,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                      const SizedBox(width: 8),
+                      const Text('Logout'),
+                    ],
+                  ),
+                ),
+              )
+            ],
+            onChanged: (value) {
+              if (value == 'logout') {
+                FirebaseAuth.instance.signOut();
+              }
+            },
+          ),
+        ],
       ),
       body: StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance.collection('chats/NoOL9MAmtXK7adMP4iMY/messages').snapshots(),
@@ -31,9 +59,7 @@ class ChatScreen extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chats/NoOL9MAmtXK7adMP4iMY/messages')
-              .add({'text': 'This was added manually'});
+          FirebaseFirestore.instance.collection('chats/NoOL9MAmtXK7adMP4iMY/messages').add({'text': 'This was added manually'});
         },
         child: const Icon(Icons.add),
       ),
